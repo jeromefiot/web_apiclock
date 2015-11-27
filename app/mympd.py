10167837ece3aa381cdee6d11a5ae1ba9cc45a01
jeromefiot <j_fiot@hotmail.com>
2015-11-27 23:20:07 +0100

    mympd pep8

--- app/mympd.py
+++ app/mympd.py
@@ -10,16 +10,21 @@ import pprint
 
 class player():
 
+    """Play arg "radio" in local HTML player."""
+
     def __init__(self):
+        """Connection init."""
         self.client = MPDClient()
         self.client.timeout = 10
         self.client.idletimeout = None
         self.client.connect("localhost", 6600)
 
     def clear(self):
+        """Clear player playlist."""
         self.client.clear()
 
     def play(self, media):
+        """Start player with media(url) arg."""
         self.client.clear()
         if media:
             self.client.add(media)
@@ -27,13 +32,16 @@ class player():
             self.client.play()
 
     def podlist(self, media):
+        """Get podcast infos(parsed)."""
         parsed = podcastparser.parse(media, urllib.urlopen(media))
         return parsed
 
     def stop(self):
+        """Stop player."""
         self.client.stop()
 
     def volup(self, n=10):
+        """Set player volume up."""
         status = self.client.status()
         nvol = int(status['volume']) + n
         if nvol > 100:
@@ -41,6 +49,7 @@ class player():
         self.client.setvol(nvol)
 
     def voldown(self, n=10):
+        """Set player volume down."""
         status = self.client.status()
         volume = status['volume']
         nvol = int(volume) - n
@@ -50,7 +59,7 @@ class player():
         print "Volume : %d" % nvol
 
     def status(self):
-
+        """Get player status."""
         status = self.client.status()
 
         monstatus = {}
@@ -68,6 +77,7 @@ class player():
         return monstatus, maplaylist
 
     def is_playing(self):
+        """Verify player playing."""
         status = self.status()
         if status['state'] == 'stop':
             return False
@@ -76,16 +86,15 @@ class player():
 
 
 def main():
-
+    """Playing media."""
     parser = argparse.ArgumentParser(
-    description="This script play a given media on the mpd (local) server")
+        description="This script play a given media on the mpd (local) server")
     parser.add_argument("-c",
         choices=['play', 'stop', 'status', 'volup', 'voldown', 'pod'],
-        required=True)
+                 required=True)
     parser.add_argument("-u", help="url to play")
 
     args = parser.parse_args()
-
     media = args.u
     command = args.c
 
